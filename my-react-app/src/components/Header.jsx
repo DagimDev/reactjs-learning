e64438d191a7e01c60f95a3745edb1cd9c30a26c
Dagim Sime <simedagim121@gmail.com>
2025-11-05 21:16:59 +0300

    feat: create header component with navigation

---
+++ my-react-app/src/components/Header.jsx
@@ -0,0 +1,16 @@
+// components/Header.js
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header
