f0a2d66dae7679c2976ec7763e173fd2831b4368
Dagim Sime <simedagim121@gmail.com>
2025-11-11 13:06:50 +0300

    docs: What is a React component’s lifecycle

---
+++ my-react-app/src/react-lifecycle-methods-class-based.jsx
@@ -0,0 +1,4 @@
+//  React component’s lifecycle methods (class based)
+    // ● What is a React component’s lifecycle? React component’s lifecycle is as the series of
+    // events that happen from the birth of a React component to its death. In short, React
+    // component lifecycle is the “lifetime” of a component.
