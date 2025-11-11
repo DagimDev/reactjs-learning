7f218d2efb03fcea7022abcc60f6594938f1759e
Dagim Sime <simedagim121@gmail.com>
2025-11-11 13:08:01 +0300

    docs: What are the four stages a React component goes through in its lifecycle

--- my-react-app/src/react-lifecycle-methods-class-based.jsx
+++ my-react-app/src/react-lifecycle-methods-class-based.jsx
@@ -2,3 +2,16 @@
     // ● What is a React component’s lifecycle? React component’s lifecycle is as the series of
     // events that happen from the birth of a React component to its death. In short, React
     // component lifecycle is the “lifetime” of a component.
+
+//  What are the four stages a React component goes through in its lifecycle?
+// ▪ Initialization: This is the stage where the component is constructed with the
+    // given Props and initial state. This is done in the constructor of a class component.
+// ▪ Mounting: This is the stage where the component is mounted on the DOM and
+    // rendered for the first time on the webpage. Mounting is the stage of rendering the
+    // JSX returned by the render method itself. This is basically the birth of a
+    // component.
+// ▪ Updating: Updating is the stage when the state of a component is updated, and
+    // the application is repainted. This is basically the growth of a component.
+// ▪ Unmounting: As the name suggests unmounting is the final step of the
+    // component lifecycle where the component is removed from the page. This is
+    // basically the death of a component.
